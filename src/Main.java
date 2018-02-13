a32b3e72325810180bf4a07d70596351c883c195
momen-ghulmi <momen.ghulmi@gmail.com>
2018-02-13 10:19:53 +0200

    Ant testing

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-import com.techpalace.calculator.view.CalculatorView;
-
-/**
- * Created by momen on 2/12/2018.
- */
-public class Main {
-
-}
---
+++ src/com/techpalace/calculator/Main.java
@@ -0,0 +1,12 @@
+package com.techpalace.calculator;
+
+import com.techpalace.calculator.view.CalculatorView;
+
+/**
+ * Created by momen on 2/12/2018.
+ */
+public class Main {
+    public static void main(String[] args){
+        CalculatorView view = CalculatorView.getInstance();
+    }
+}
